2883cca06af70425677d90ddab7d0fb88214fdaf
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2024-01-14 19:28:53 +0545

    week 7 initial commit

---
+++ week-7/week-07-routing/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>React App</h1>
+        <p>React is working!</p>
+      </div>
+    </div>
+  );
+};
+export default App;
